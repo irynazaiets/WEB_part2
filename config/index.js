773ac5494a845bc96788f08a4f2efd3a3f4f77d5
zaiets <irynazaiets98@ukr.net>
2019-06-02 20:57:20 +0300

    First commit

---
+++ config/index.js
@@ -0,0 +1,19 @@
+﻿'use strict'
+
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv');
+  dotenv.config()
+}
+
+const env = process.env;
+
+module.exports = {
+  host: env.HOST || 8080,
+  port: env.PORT || 'localhost',
+  dbschema: env.DBSCHEMA || 'test',
+  dbport: env.DBPORT,
+  dbuser: env.DBUSER || 'zaytsir',
+  dbpwd: env.DBPWD || '2708648913',
+  dbhost: env.DBHOST,
+  dbname: env.DBNAME || 'test'
+};
---
+++ services/product.create.js
@@ -0,0 +1,21 @@
+const Product = require('./../models/product')
+
+/**
+ * @param {Object} data
+ */
+module.exports = function (data) {
+  const product = new Product({
+    name: data.name,
+    weight: data.weight,
+    volume: data.volume
+  })
+  return new Promise((resolve, reject) => {
+    product.save(function (err, createdProduct) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(createdProduct)
+      }
+    })
+  })
+}
